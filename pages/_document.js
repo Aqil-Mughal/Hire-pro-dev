9acf632f0218f0d1430f2ba0851f488076d9d9fe
Omer Nadeem <omernadeem@aamanto.com>
2023-06-27 14:07:36 +0500

    Font changed to Quicksand

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,10 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         />
+
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+
+
         <link rel="stylesheet" href="https://kendo.cdn.telerik.com/themes/5.10.0/default/default-ocean-blue.css" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
@@ -20,7 +24,7 @@ export default function Document() {
         />
         <meta name="ibthemes" content="ATFN" />
 
-        <link rel="icon" href="/favico.png" type="image/png"/>
+        <link rel="icon" href="/favico.png" type="image/png" />
       </Head>
       <body>
         <Main />
